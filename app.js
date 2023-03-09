d4ce26eb1913362a255e666c7ea323a4b16728d0
dhy <harsh612y@gmail.com>
2023-03-09 18:44:15 +0530

    Add design popup-window

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.mobile-menu');
 const menuItems = document.querySelectorAll('.menuItem');
-const hamburgerBtn = document.querySelector('.hambug-menu');
+const hamburger = document.querySelector('.hambug-menu');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if (mobileMenu.classList.contains('showMenu')) {
-    mobileMenu.classList.remove('showMenu');
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
     menuIcon.style.display = 'block';
   } else {
-    mobileMenu.classList.add('showMenu');
+    menu.classList.add('showMenu');
     closeIcon.setAttribute('style', 'display: block !important');
     menuIcon.style.display = 'none';
   }
 }
 
-hamburgerBtn.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', toggleMenu);
-});
+menuItems.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
 
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach((link) => {
